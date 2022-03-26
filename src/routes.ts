b395c57f96366e00509350372f505c91a16f90c9
Fernando Lins <fernandolins088@gmail.com>
2022-03-26 20:23:44 -0300

    Add route to create delivery

--- src/routes.ts
+++ src/routes.ts
@@ -4,17 +4,22 @@ import CreateClientController from './modules/client/useCases/createClient/creat
 import AuthenticateClientController from './modules/account/autheticateClient/AuthenticateClientController'
 import CreateDeliverymanController from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 import AuthenticateDeliverymanController from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController'
+import CreateDeliveryController from './modules/delivery/useCases/createDelivery/CreateDeliveryController'
+import ensureAuthenticatedClient from './middlewares/ensureAuthenticatedClient'
 
 const routes = Router()
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/client', createClientController.handle)
 routes.post('/client/authenticate', authenticateClientController.handle)
 
 routes.post('/deliveryman', createDeliverymanController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
+
+routes.post('/delivery', ensureAuthenticatedClient, createDeliveryController.handle)
 
 export default routes
